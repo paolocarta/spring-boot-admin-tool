7a2144f7a210ff8799e70cef0720bfc90c067f65
paolo carta <>
2018-06-06 18:14:13 +0200

    adding EnableAdminServer annotation

--- src/main/java/ch/interdiscount/showcaseadmintool/DemoApplication.java
+++ src/main/java/ch/interdiscount/showcaseadmintool/DemoApplication.java
@@ -1,9 +1,11 @@
 package ch.interdiscount.showcaseadmintool;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
